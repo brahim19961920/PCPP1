4dbbfb8e7c2d66b979d845c05ac34c8bcb08bfb3
bbenamira <brahim.benamira@amadeus.com>
2023-07-19 20:34:58 +0200

    Fix randint start parameter

--- GUI/lab2.py
+++ GUI/lab2.py
@@ -7,7 +7,7 @@ def move_button(event=None):
     old_abs, old_ord = button_abs, button_ord
 
     while abs(old_abs - button_abs) < 100 and abs(old_ord - button_ord) < 100:
-        button_abs, button_ord = randint(200, 400), randint(200, 400)
+        button_abs, button_ord = randint(0, 400), randint(0, 400)
 
     button.place(x=button_abs, y=button_ord)
 
